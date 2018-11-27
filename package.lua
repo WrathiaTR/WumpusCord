666f73f3d03192577b1777e50bafbccebfe9cdf5
Prylaris <prylaris.git@gmail.com>
2018-11-27 12:11:37 -0500

    add deps

---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+	name = 'Prylaris/discordlua',
+	version = '1.2.0',
+	homepage = 'https://github.com/Prylaris/discordlua',
+	dependencies = {
+		'creationix/coro-http@3.0.0',
+		'creationix/coro-websocket@3.1.0',
+		'luvit/secure-socket@1.2.0',
+	},
+	tags = { 'lua', 'lib' },
+	license = 'MIT',
+	author = 'Prylaris',
+	files = { '**.lua', '**.yml', '**.yaml' },
+}
